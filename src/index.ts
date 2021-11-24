e118f7da84d5704d4736dc580b4510f21ade81f1
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-11-24 19:26:55 -0300

    feat: start express server

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,14 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import * as express from 'express'
 
-createConnection().then(async connection => {
+import routes from './routes'
+import './domain/connect'
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+const app = express()
+const port = process.env.PORT || 5000
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+app.use(express.json())
+app.use('/api',routes)
+app.listen(port, () => console.log(`Server is running on port ${port}`))
 
-    console.log("Here you can setup and run express/koa/any other framework.");
-
-}).catch(error => console.log(error));
+export default app
