0c1513659671ca20fbf224d8af7f84a7dce0eeaa
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-11-25 18:40:48 -0300

    feat: add new method to create a note

--- src/controllers/NoteController.ts
+++ src/controllers/NoteController.ts
@@ -26,6 +26,30 @@ class NoteController{
             })
         }
     }
+
+    async createNote(req: Request, res: Response<IResponse>): Promise<Response<IResponse>> {
+        try{
+            const noteRepository = getRepository(Note)
+            const createdNote = noteRepository.create(req.body)
+            await noteRepository.save(createdNote)
+
+            return res.json({
+                status: ResponseStatus.OK,
+                data: createdNote
+            })
+        }catch(error){
+            if(error instanceof ValidationError){
+                return res.status(400).json({
+                    status: ResponseStatus.BAD_REQUEST,
+                    errors: error.errors
+                })
+            }
+            return res.status(500).json({
+                status: ResponseStatus.INTERNAL_SERVER_ERROR,
+                message: 'An internal server error has happened.'
+            })
+        }
+    }
 }
 
 export default new NoteController()
