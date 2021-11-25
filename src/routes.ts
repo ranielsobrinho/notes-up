042e51cca90d6cb0142ec3c44bb5f4e9f01b8d74
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-11-25 18:13:53 -0300

    feat: add new routes endpoints

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,14 @@
 import { Router } from 'express'
+import { UserController, NoteController } from './controllers/'
 
 const routes = Router()
+
+routes.get('/users', UserController.getUsers)
+routes.get('/users/:id', UserController.getOneUser)
+routes.post('/users', UserController.createUser)
+routes.put('/users/:id', UserController.updateUser)
+routes.delete('/users/:id', UserController.deleteUser)
+
+routes.get('/notes', NoteController.getNotes)
 
 export default routes
