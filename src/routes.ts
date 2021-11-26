530b0236c1f35bea7b21da266b7be0b6dda3b772
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-11-26 08:09:55 -0300

    feat: add yup schemas to validate input data in routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,20 @@
 import { Router } from 'express'
+
 import { UserController, NoteController } from './controllers/'
+import { userSchema, noteSchema } from './utils/validations'
+import validate from './middlewares/validationMiddleware'
 
 const routes = Router()
 
 routes.get('/users', UserController.getUsers)
 routes.get('/users/:id', UserController.getOneUser)
-routes.post('/users', UserController.createUser)
-routes.put('/users/:id', UserController.updateUser)
+routes.post('/users', validate(userSchema), UserController.createUser)
+routes.put('/users/:id', validate(userSchema), UserController.updateUser)
 routes.delete('/users/:id', UserController.deleteUser)
 
 routes.get('/notes', NoteController.getNotes)
-routes.post('/notes', NoteController.createNote)
-routes.put('/notes/:id', NoteController.updateNote)
+routes.post('/notes', validate(noteSchema) ,NoteController.createNote)
+routes.put('/notes/:id', validate(noteSchema) ,NoteController.updateNote)
 routes.delete('/notes/:id', NoteController.deleteTodo)
 
 export default routes
