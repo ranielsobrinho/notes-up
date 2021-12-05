0f0efd609ab99f59dcf8c5ec475b04847d63e5e1
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-05 10:50:14 -0300

    feat: add auth validation and a new route

--- src/routes.ts
+++ src/routes.ts
@@ -1,20 +1,23 @@
 import { Router } from 'express'
 
-import { UserController, NoteController } from './controllers/'
+import { UserController, NoteController, AuthController } from './controllers/'
 import { userSchema, noteSchema } from './utils/validations'
-import validate from './middlewares/validationMiddleware'
+import { validate, AuthMiddleware } from './middlewares/'
 
 const routes = Router()
 
 routes.get('/users', UserController.getUsers)
 routes.get('/users/:id', UserController.getOneUser)
 routes.post('/users', validate(userSchema), UserController.createUser)
-routes.put('/users/:id', validate(userSchema), UserController.updateUser)
-routes.delete('/users/:id', UserController.deleteUser)
+routes.put('/users/:id', validate(userSchema), AuthMiddleware.auth, UserController.updateUser)
+routes.delete('/users/:id', AuthMiddleware.auth ,UserController.deleteUser)
 
 routes.get('/notes', NoteController.getNotes)
-routes.post('/notes', validate(noteSchema) ,NoteController.createNote)
-routes.put('/notes/:id', validate(noteSchema) ,NoteController.updateNote)
-routes.delete('/notes/:id', NoteController.deleteTodo)
+routes.get('/notes/:id', NoteController.getNote)
+routes.post('/notes', validate(noteSchema) , AuthMiddleware.auth, NoteController.createNote)
+routes.put('/notes/:id', AuthMiddleware.auth , NoteController.updateNote)
+routes.delete('/notes/:id', AuthMiddleware.auth ,NoteController.deleteTodo)
+
+routes.post('/auth', validate(userSchema), AuthController.authenticate)
 
 export default routes
