1c5beb2a1ba0ac0e6ece2ba227adf21fd0f30739
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-05 10:38:30 -0300

    feat: add cors config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import * as express from 'express'
+import * as cors from 'cors'
 
 import routes from './routes'
 import './domain/connect'
@@ -7,6 +8,13 @@ import './domain/connect'
 const app = express()
 const port = process.env.PORT || 5000
 
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  })
+)
 app.use(express.json())
 app.use('/api',routes)
 app.listen(port, () => console.log(`Server is running on port ${port}`))
