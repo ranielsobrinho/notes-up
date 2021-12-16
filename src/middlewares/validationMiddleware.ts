f4ccdc4ba490d0519f3a9c26752d735346e59d91
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-16 11:48:15 -0300

    feat: add yup validation middleware

--- src/middlewares/validationMiddleware.ts
+++ src/middlewares/validationMiddleware.ts
@@ -1,16 +1,25 @@
 import { Request, Response, NextFunction } from 'express'
 import * as yup from 'yup'
 
-const validate = (schema) => async ( req: Request, res: Response, next: NextFunction ) => {
-    try{
-        const success = await schema.validate(req.body)
-        if(!success){
-            return res.json({ message: 'There is some empty required field.' })
-        }
-        next()
-    }catch(error){
-        return res.json(error)
-    }
+const validate = (schema) => async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { body } = req
+  try {
+    schema
+      .validate(body)
+      .then()
+      .catch((error: { path: any; message: any }) =>
+        res.status(400).json({
+          [error.path]: error.message,
+        })
+      )
+    return next()
+  } catch (error) {
+    return res.json(error)
+  }
 }
 
 export default validate
