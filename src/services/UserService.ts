d7391163ae5da865e2144029d06bffa9e3c7ec92
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-16 15:31:47 -0300

    feat: code formatted and add request to update username and password separately

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,20 +2,20 @@ import { getRepository } from 'typeorm'
 import { User } from '../domain/entity/User'
 import * as bcrypt from 'bcrypt'
 
-interface UserRequest{
+interface UserRequest {
   username: string
   password: string
 }
 class UserService {
-  async getAll(){
+  async getAll() {
     const repo = getRepository(User)
     const users = await repo.find()
     return users
   }
 
-  async getOne(id: string){
+  async getOne(id: string) {
     const repo = getRepository(User)
-    if(!await repo.findOne(id)){
+    if (!await repo.findOne(id)) {
       return new Error('User does not exists.')
     }
     const user = await repo.findOne(id, {
@@ -24,9 +24,9 @@ class UserService {
     return user
   }
 
-  async createUser({username, password}: UserRequest): Promise<User | Error>{
+  async createUser({ username, password }: UserRequest): Promise<User | Error> {
     const repo = getRepository(User)
-    if(await repo.findOne({username})){
+    if (await repo.findOne({ username })) {
       return new Error('User already exists. Choose another username.')
     }
 
@@ -38,6 +38,24 @@ class UserService {
     })
     await repo.save(user)
     return user
+  }
+
+  async updateUsername(id: string, username: string) {
+    const repo = getRepository(User)
+    if (!await repo.findOne(id)) {
+      return new Error('This user does not exists.')
+    }
+    const updated = repo.update(id, { username })
+    return updated
+  }
+
+  async updatePassword({ username, password }: UserRequest) {
+    const repo = getRepository(User)
+    if (!await repo.findOne({ username })) {
+      return new Error('There is no user with this username.')
+    }
+    const updated = await repo.update(username, { password })
+    return updated
   }
 }
 
