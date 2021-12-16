4e4ea4ac1ca2bdaf24c72419c81f743cdd912da6
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-16 16:20:08 -0300

    feat: add file to export all methods

---
+++ src/controllers/UserController/index.ts
@@ -0,0 +1,13 @@
+import GetUsers from "./GetUsers";
+import GetUser from "./GetUser";
+import CreateUser from "./CreateUser";
+import UpdateUser from "./UpdateUser";
+import DeleteUser from "./DeleteUser";
+
+export {
+  GetUser,
+  GetUsers,
+  CreateUser,
+  UpdateUser,
+  DeleteUser
+}
