0501bd4d2d0b4f6e9bc2c94a2c43a40be3b2b0ba
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-01-18 08:39:19 -0300

    refactor: import and use new controller methods

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { NoteController, AuthController } from './controllers/'
 import { userSchema, noteSchema } from './utils/validations'
 import { validate, AuthMiddleware } from './middlewares/'
 import { GetUsers, GetUser, CreateUser, UpdateUser, DeleteUser } from './controllers/UserController/'
+import { CreateNote, DeleteNote, GetNote, GetNotes, UpdateNote } from './controllers/NoteController'
 
 const routes = Router()
 
@@ -13,11 +14,11 @@ routes.post('/users', validate(userSchema), CreateUser.execute)
 routes.put('/users/:id', validate(userSchema), AuthMiddleware.auth, UpdateUser.execute)
 routes.delete('/users/:id', AuthMiddleware.auth, DeleteUser.execute)
 
-routes.get('/notes', NoteController.getNotes)
-routes.get('/notes/:id', NoteController.getNote)
-routes.post('/notes', validate(noteSchema), NoteController.createNote)
-routes.put('/notes/:id', NoteController.updateNote)
-routes.delete('/notes/:id', NoteController.deleteTodo)
+routes.get('/notes', GetNotes.execute)
+routes.get('/notes/:id', GetNote.execute)
+routes.post('/notes', validate(noteSchema), CreateNote.execute)
+routes.put('/notes/:id', UpdateNote.execute)
+routes.delete('/notes/:id', DeleteNote.execute)
 
 routes.post('/auth', validate(userSchema), AuthController.authenticate)
 
