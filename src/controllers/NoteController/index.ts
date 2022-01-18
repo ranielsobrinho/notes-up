c859607ff894f797e8e65cc141da54ef498fa210
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-01-18 08:38:52 -0300

    refactor: add file to export all note controller methods

---
+++ src/controllers/NoteController/index.ts
@@ -0,0 +1,13 @@
+import CreateNote from "./CreateNote";
+import DeleteNote from "./DeleteNote";
+import GetNotes from "./GetNotes";
+import GetNote from "./GetNote";
+import UpdateNote from "./UpdateNote";
+
+export {
+  CreateNote,
+  DeleteNote,
+  GetNote,
+  GetNotes,
+  UpdateNote
+}
